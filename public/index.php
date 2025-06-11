179cab266a35b050c312f3970afe38d4032a15a5
Nattapatpon02 <nattapatpon100645@gmail.com>
2025-06-12 00:53:27 +0700

    test

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
+<?php
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Home</title>
 </head>
+
 <body>
-    
-    
+
+    <H1>HELLO</H1>
+    <img src="assets/img/RJ-Pasin.png" alt="">
 </body>
+
 </html>
